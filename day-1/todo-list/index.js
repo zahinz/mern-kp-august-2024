c44129f90972ab852eed84680004e78266fa415b
zahinz <dev.huxnet@gmail.com>
2024-12-23 22:55:31 +0800

    params vs body

--- day-1/todo-list/index.js
+++ day-1/todo-list/index.js
@@ -24,6 +24,35 @@ app.post("/", function (req, res) {
   return res.json(data);
 });
 
+// params -> GET
+// if the dynamic data only 1 and GET -> PARAMS
+app.get("/:username", function (req, res) {
+  console.log(req.params);
+  const username = req.params.username;
+  const data = {
+    message: "User data received",
+    data: username,
+  };
+  return res.json(data);
+});
+
+// dynamic data more than 2
+// form
+// POST & body
+app.post("/bmi", function (req, res) {
+  const weight = req.body.weight;
+  const height = req.body.height;
+  if (!height || !weight) {
+    return res.status(400).json("Invalid request");
+  }
+  const bmi = weight / Math.pow(height, 2);
+  const data = {
+    message: "bmi user received",
+    data: bmi,
+  };
+  return res.json(data);
+});
+
 app.listen(PORT, function () {
   console.log(
     `My server run at port ${PORT}. Open at browser http://localhost:8080`
